0a50bed2ad7eadcea7f176110023c7bafc6b67b0
Ducemon <72726779+Ducemon@users.noreply.github.com>
2022-04-19 17:26:33 +0300

    Add files via upload

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,13 +2,6 @@ require("@nomiclabs/hardhat-waffle");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -18,4 +11,16 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  paths:{
+    sources:"./contracts",
+    tests:"./test",
+    cache:"./cache",
+    artifacts:"./artifacts"
+  },
+  networks:{
+    local:{
+      url: "http://localhost:8545/",
+      accounts: ["0x92db14e403b83dfe3df233f83dfa3a0d7096f21ca9b0d6d6b8d88b2b4ec1564e"]
+    }
+  }
 };
