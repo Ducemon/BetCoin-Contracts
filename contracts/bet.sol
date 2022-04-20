911c120edcbefb8a95db426e1f551e39f4bdea8c
Sina Sorin <sorin.sina@mountsoftware.ro>
2022-04-20 15:19:41 +0300

    fixes

--- contracts/bet.sol
+++ contracts/bet.sol
@@ -6,59 +6,50 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract Bet {
 
     address user;
-    address bank = 0x0a20cA23a99488cb3220d3125F39135e4bDa8063;
+    address bank = 0xdD2FD4581271e230360230F9337D5c0430Bf44C0;
     uint256 bet_id;
     uint256 bet_odd;
     uint256 bet_amount;
     ERC20 token;
 
-
-
-
-    constructor(uint256 id, uint256 odd, address token_address){
-        user = msg.sender;
+    constructor(uint256 id, uint256 odd, address token_address, address sender){
+        user = sender;
         bet_id = id;
         bet_odd = odd;
         token = ERC20(token_address);
     }
 
-    function getContractTokenBalance() public view returns(uint256){
+    function getContractTokenBalance() public view returns (uint256){
         return token.balanceOf(address(this));
-   }
-
+    }
 
     function bet(uint256 amount) public returns (bool){
 
         bet_amount = amount;
-        require(amount>0, "Trebuie sa joci ceva");
+        require(amount > 0, "Trebuie sa joci ceva");
         require(msg.sender == user, "Pariaza pe pariurile tale");
-        require(token.balanceOf(msg.sender)>bet_amount);
+        require(token.balanceOf(msg.sender) > bet_amount);
         token.approve(address(this), bet_amount);
         token.transfer(address(this), bet_amount);
-        
+
         return true;
     }
 
-    function fuelBet() public{
-            require(msg.sender == bank);
-            bet_amount = bet_amount*bet_odd;
-            token.approve(address(this), bet_amount);
-            token.transfer(address(this), bet_amount);
+    function fuelBet() public {
+        require(msg.sender == bank);
+        bet_amount = bet_amount * bet_odd;
+        token.approve(address(this), bet_amount);
+        token.transfer(address(this), bet_amount);
 
     }
 
-    function betWin() public{
+    function betWin() public {
         token.transferFrom(address(this), user, bet_amount);
 
     }
 
-    function betLose() public{
+    function betLose() public {
         token.transferFrom(address(this), bank, bet_amount);
     }
 
-
-
-
-
-    
 }
--- contracts/betfactory.sol
+++ contracts/betfactory.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
+
 import "./bet.sol";
-
-
 
 
 contract BetFactory {
 
 
     function newBet(uint256 id, uint256 odd, address token_address) public returns (address){
-        Bet bet = new Bet(id, odd, token_address);
+        Bet bet = new Bet(id, odd, token_address, msg.sender);
         return address(bet);
     }
+
 }
